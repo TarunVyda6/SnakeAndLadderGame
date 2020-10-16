59b2a6eb737041f3a41e3741e99b092a2c171500
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-10-16 09:00:55 +0530

    player option check feature is added

--- SnakeAndLadderSimulator.java
+++ SnakeAndLadderSimulator.java
@@ -4,18 +4,38 @@ public class SnakeAndLadderSimulator
 {
 	public static void main(String[] args) 
 	{
+		Random random = new Random();
 		int playerPosition = 0;
-		int dieNumber = dieRoll();
+		int dieNumber = dieRoll( random );
+		playerPosition = playerOptionCheck( random, playerPosition, dieNumber );
 	}
 	
 	// uc1
-	public static int dieRoll()
+	public static int dieRoll( Random random )
 	{
-		Random random = new Random();
+		
 		int dieNumber = random.nextInt(6) + 1;
 		return dieNumber;
 	}
 	
+	// uc2
+	public static int playerOptionCheck( Random random, int playerPosition, int dieNumber )
+	{
+		final int noPlay = 0;
+		final int ladder = 1;
+		final int snake = 2;
+		
+		int playerOption = random.nextInt(3);
+		switch( playerOption )
+		{
+			case	noPlay	:	playerPosition = playerPosition;
+								break;
+			case	ladder	:	playerPosition = playerPosition + dieNumber;
+								break;
+			case	snake	:	playerPosition = playerPosition - dieNumber;
+		}
+		return playerPosition;
+	}
 	
 }
 
