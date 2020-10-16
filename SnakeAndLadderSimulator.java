2465afb33132a24cb6f654360d600a666be6ec02
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-10-16 08:29:05 +0530

    die roll feature is added

--- SnakeAndLadderSimulator.java
+++ SnakeAndLadderSimulator.java
@@ -1,4 +1,21 @@
+import java.util.Random;
+
 public class SnakeAndLadderSimulator
 {
-	int playerPosition = 0;
+	public static void main(String[] args) 
+	{
+		int playerPosition = 0;
+		int dieNumber = dieRoll();
+	}
+	
+	// uc1
+	public static int dieRoll()
+	{
+		Random random = new Random();
+		int dieNumber = random.nextInt(6) + 1;
+		return dieNumber;
+	}
+	
+	
 }
+
